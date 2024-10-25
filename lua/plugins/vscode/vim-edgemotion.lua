03f284ad76044309302ebbffa42d114de67f1ade
yscurea <yuuki.990510@gmail.com>
2024-10-25 10:17:59 +0900

    Remap: conflict keymap

--- lua/plugins/vscode/vim-edgemotion.lua
+++ lua/plugins/vscode/vim-edgemotion.lua
@@ -2,7 +2,7 @@ return {
   "haya14busa/vim-edgemotion",
   lazy = true,
   keys = {
-    { "<Leader>J", "<Plug>(edgemotion-j)", desc = "jump edge down", mode = { "n", "v" } },
-    { "<Leader>K", "<Plug>(edgemotion-k)", desc = "jump edge up",   mode = { "n", "v" } },
+    { "<Leader>jj", "<Plug>(edgemotion-j)", desc = "jump edge down", mode = { "n", "v" } },
+    { "<Leader>kk", "<Plug>(edgemotion-k)", desc = "jump edge up",   mode = { "n", "v" } },
   },
 }
